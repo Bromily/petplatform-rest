13e4e647c6aebfc652637e0333b3c12fe6ff275a
haksung59 <hak9501@gmail.com>
2022-07-20 16:43:08 +0900

    create project

---
+++ src/main/java/com/petplatform/PetplatformApplication.java
@@ -0,0 +1,13 @@
+package com.petplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetplatformApplication.class, args);
+    }
+
+}
