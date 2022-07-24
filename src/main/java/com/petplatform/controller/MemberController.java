d689bd858a3de0750484d8046e50c7d59eba5c3b
haksung59 <hak9501@gmail.com>
2022-07-24 13:59:05 +0900

    branch test

--- src/main/java/com/petplatform/controller/MemberController.java
+++ src/main/java/com/petplatform/controller/MemberController.java
@@ -9,16 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
-    @Autowired
+    @Autowired  //mapper를 자동으로 연결? 정확한 정의 모름
     public MemberMapper memberService;
 
-    @GetMapping("/login")
+    @GetMapping("/login")   // get타입으로 매핑
     public MemberDto selectMember(){
+        //매퍼에서 selectMember 함수를 실행시켜서 받아온 MemberDto를 member1 객체로 선언
         MemberDto member1 = memberService.selectMember("haksung59");
 
         System.out.println("hi2");
 
-        return member1;
+        return member1; // MemberDto객체 member1 반환
     }
 
 }
