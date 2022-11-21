daa5788d165ef850811aac5c02b90628bad3af7f
haksung59 <hak9501@gmail.com>
2022-11-21 13:05:28 +0900

    찬익이 test용

---
+++ src/main/java/com/petplatform/mapper/CommonMapper.java
@@ -0,0 +1,12 @@
+package com.petplatform.mapper;
+
+import com.petplatform.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommonMapper {
+
+    UserDto getUserInfo(String id);
+    UserDto postUserInfo(UserDto userDto);
+
+}
