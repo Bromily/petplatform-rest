c6eb4f6e9f94b30d32891ed3b39b62f3c10b594c
ANCHANIK <cksdlr7446@gmail.com>
2022-11-22 22:29:47 +0900

    react setting

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    fetch("/user?id=haksung59")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{message.name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
       </header>
     </div>
   );
