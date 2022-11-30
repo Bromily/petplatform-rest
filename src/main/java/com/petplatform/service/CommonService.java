7d03b6779e51b861252baefdd0504f15b7f435db
haksung59 <hak9501@gmail.com>
2022-11-30 16:15:40 +0900

    sha256 분리에 따른 exception처리 삭제

--- src/main/java/com/petplatform/service/CommonService.java
+++ src/main/java/com/petplatform/service/CommonService.java
@@ -3,11 +3,8 @@ package com.petplatform.service;
 import com.petplatform.common.SHA256;
 import com.petplatform.dto.UserDto;
 import com.petplatform.mapper.CommonMapper;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.security.NoSuchAlgorithmException;
 
 @Service
 public class CommonService {
@@ -15,7 +12,7 @@ public class CommonService {
     @Autowired
     public CommonMapper mapper;
 
-    public UserDto signIn(UserDto userDto) throws NoSuchAlgorithmException {
+    public UserDto signIn(UserDto userDto) {
 
         SHA256 sha256 = new SHA256();
 
