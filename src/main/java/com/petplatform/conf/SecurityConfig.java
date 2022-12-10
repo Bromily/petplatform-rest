c1b10cde1f99df97b1dd3eef6e7aeb0d2af44daf
haksung59 <hak9501@gmail.com>
2022-12-11 01:18:42 +0900

    반환 타입 정형화

--- src/main/java/com/petplatform/conf/SecurityConfig.java
+++ src/main/java/com/petplatform/conf/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.petplatform.conf;
 
+import com.petplatform.auth.AuthenticationEntryPointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
